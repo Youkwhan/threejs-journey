b80aae73051dc5ca528a36e7e11aee28db7d36da
Youkwhan <Youkwhan@gmail.com>
2024-03-24 19:01:47 -0400

    Grouping Mesh together, Scene graph

--- lessons/04/04-transform-objects/src/script.js
+++ lessons/04/04-transform-objects/src/script.js
@@ -9,29 +9,58 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
 // mesh.position.x = 0.7;
 // mesh.position.y = -0.6;
 // mesh.position.z = 1;
-scene.add(mesh);
+// scene.add(mesh);
+
+// == SCENE GRAPH ==
+// You can put objects inside groups
+// Group class is an instance of a Object3D
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
 
 // == Position ==
 // The position property is not an object, but an instance of the `Vector3` class.
 // length of a vector (distance from the center 0,0)
-console.log(mesh.position.length());
+// console.log(mesh.position.length());
 //Normazlie vector values (meaning you will reduce the length of the vector to 1 unit, but preserve its direction)
-console.log(mesh.position.normalize());
+// console.log(mesh.position.normalize());
 // To change the vector3 values
-mesh.position.set(0.7, -0.6, 1);
+// mesh.position.set(0.7, -0.6, 1);
 
 // == Scale ==
 // instance of the Vector3 class
 // mesh.scale.x = 2;
 // mesh.scale.y = 0.25;
 // mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
+// mesh.scale.set(2, 0.5, 0.5);
 
 // == Rotate ==
 // With `rotation` or with `quarternion`.
@@ -41,9 +70,9 @@ mesh.scale.set(2, 0.5, 0.5);
 // Params: x,y,x (optional) the angle of rotation in radians around THAT/THIS axis.
 // - order(optional): order that the rotation are applied, default 'XYZ'.
 // !! Half a roation is Math.PI, 3.14159. A = 2Pi*r (r rotating 2PI times)
-mesh.rotation.reorder('YXZ');
-mesh.rotation.y = Math.PI * 0.25;
-mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.reorder('YXZ');
+// mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.x = Math.PI * 0.25;
 // Careful of `gimbal lock`, order of rotation important. i.e. FPS game our head rotates on the y before x to look around.
 
 // Quaterion (Fixes a lot of problems ordering), also expresses a rotation, but in a more mathematical way, matrices.
@@ -73,10 +102,15 @@ scene.add(camera);
 
 // Object3D instances have a lookAt(...) method which rotates the object so that its -z faces te target provided (target: must be a Vector3)
 // lookAt rorates around its CURRENT POSITION to face the target specified.
-camera.lookAt(mesh.position);
+// camera.lookAt(mesh.position);
 
 // Distance from another Vector3
-console.log(mesh.position.distanceTo(camera.position));
+// console.log(mesh.position.distanceTo(camera.position));
+
+/* COMBINING TRANSFORMATION
+You can combine position, rotation (or quaternion), and scale in ANY order!
+- Think about it as a state before we render it on the canvas.
+*/
 
 /**
  * Renderer
