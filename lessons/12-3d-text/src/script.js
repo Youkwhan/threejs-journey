4daf83179763fb973a20ab42a52a61cc6547ba09
Youkwhan <Youkwhan@gmail.com>
2024-06-22 17:39:16 -0400

    optimization

--- lessons/12-3d-text/src/script.js
+++ lessons/12-3d-text/src/script.js
@@ -23,7 +23,7 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
-const matcapTexture = textureLoader.load('/textures/matcaps/5.png');
+const matcapTexture = textureLoader.load('/textures/matcaps/7.png');
 matcapTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
@@ -53,17 +53,14 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
   //   ); OR USE:
   textGeometry.center();
 
-  const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
   //   textMaterial.wireframe = true;
-  const text = new THREE.Mesh(textGeometry, textMaterial);
+  const text = new THREE.Mesh(textGeometry, material);
   scene.add(text);
 
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
   for (let i = 0; i < 100; i++) {
-    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
-    const donutMaterial = new THREE.MeshMatcapMaterial({
-      matcap: matcapTexture,
-    });
-    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+    const donut = new THREE.Mesh(donutGeometry, material);
 
     // randomness in their position
     donut.position.x = (Math.random() - 0.5) * 10;
