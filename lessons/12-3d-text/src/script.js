d26ce28a374f0fe492eac52a3e1389c07a1af7d1
Youkwhan <Youkwhan@gmail.com>
2024-06-22 16:47:42 -0400

    center the text

--- lessons/12-3d-text/src/script.js
+++ lessons/12-3d-text/src/script.js
@@ -15,6 +15,10 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
+//Axis helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
 /**
  * Textures
  */
@@ -37,6 +41,16 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
     bevelOffset: 0,
     bevelSegments: 4,
   });
+
+  // bounding our object and using cords to center
+  //   textGeometry.computeBoundingBox();
+  //   textGeometry.translate(
+  //     -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+  //     -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+  //     -(textGeometry.boundingBox.max.z - 0.03) * 0.5
+  //   ); OR USE:
+  textGeometry.center();
+
   const textMaterial = new THREE.MeshBasicMaterial();
   textMaterial.wireframe = true;
   const text = new THREE.Mesh(textGeometry, textMaterial);
