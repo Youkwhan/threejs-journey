9a5660e0676670e68f08841a82bcee98ff96757a
Youkwhan <Youkwhan@gmail.com>
2024-07-23 23:40:53 -0400

    roof textures

--- lessons/16-haunted-house/src/script.js
+++ lessons/16-haunted-house/src/script.js
@@ -80,6 +80,27 @@ const wallNormalTexture = textureLoader.load(
 // Always do this with color textures
 wallColorTexture.colorSpace = THREE.SRGBColorSpace;
 
+// Roof
+const roofColorTexture = textureLoader.load(
+  './roof/roof_slates_02_1k/roof_slates_02_diff_1k.jpg'
+);
+const roofARMTexture = textureLoader.load(
+  './roof/roof_slates_02_1k/roof_slates_02_arm_1k.jpg'
+);
+const roofNormalTexture = textureLoader.load(
+  './roof/roof_slates_02_1k/roof_slates_02_nor_1k.jpg'
+);
+
+roofColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+roofColorTexture.repeat.set(3, 1);
+roofARMTexture.repeat.set(3, 1);
+roofNormalTexture.repeat.set(3, 1);
+
+roofColorTexture.wrapS = THREE.RepeatWrapping;
+roofARMTexture.wrapS = THREE.RepeatWrapping;
+roofNormalTexture.wrapS = THREE.RepeatWrapping;
+
 /**
  * House
  */
@@ -151,7 +172,13 @@ house.add(walls);
 // Roof
 const roof = new THREE.Mesh(
   new THREE.ConeGeometry(3.5, 1.5, 4),
-  new THREE.MeshStandardMaterial()
+  new THREE.MeshStandardMaterial({
+    map: roofColorTexture,
+    aoMap: roofARMTexture,
+    roughnessMap: roofARMTexture,
+    metalnessMap: roofARMTexture,
+    normalMap: roofNormalTexture,
+  })
 );
 roof.position.y = 2.5 + 0.75;
 roof.rotation.y = Math.PI * 0.25;
